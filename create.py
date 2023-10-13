a363c827379e42607ad4ca79b1b1a8c560b5b8d3
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-10-13 12:41:56 +0200

    Create the SEGMENT and POWERTRAIN code lists

--- create.py
+++ create.py
@@ -1,8 +1,31 @@
 import pandas as pd
 import sdmx
 import sdmx.message as msg
+import sdmx.model.v21 as model
 
 FILENAME = "supplementary_information_gfei2023_tdc.xlsx"
+
+# Static structural information
+SEGMENT = {
+    "large car": "Large car",
+    "large suv": "Large SUV",
+    "lcv": "Light commercial vehicle",
+    "medium car": "Medium car",
+    "small car": "Small car",
+    "small suv": "Small SUV",
+    "unclassified": "unclassified",
+}
+
+POWERTRAIN = {
+    "ev": "Battery electric vehicle",
+    "hv": "Hybrid vehicle",
+    "ice": "Internal combusion engine vehicle",
+    "phev": "Plug-in hybrid vehicle",
+    "fcv": "Fuel cell vehicle",
+    "mhv": "Mild-hybrid vehicle",
+    "unclassified": "unclassified",
+}
+
 
 def create_structures() -> msg.StructureMessage:
     """Create data structures."""
@@ -10,8 +33,19 @@ def create_structures() -> msg.StructureMessage:
 
     # TODO Create the maintainer agency
     # TODO Create the AREA code list
-    # TODO Create the SEGMENT code list
-    # TODO Create the POWERTRAIN code list
+
+    # Create the SEGMENT and POWERTRAIN code lists using static data
+    for codelist_id, codes in ("SEGMENT", SEGMENT), ("POWERTRAIN", POWERTRAIN):
+        # Create a code list
+        cl = model.Codelist(id=codelist_id, is_external_reference=False, is_final=True)
+
+        # Add codes
+        for id, name in codes.items():
+            cl.append(model.Code(id=id, name=name))
+
+        # Store in the message
+        sm.add(cl)
+
     # TODO Create the YEAR code list
     # TODO Create the concept scheme with the measures
     # TODO Create the data structure definition(s) and data frame(s)
